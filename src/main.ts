ec309680bd6fa01be6a605e2eed5b977fa87ae9d
Woo Tae Hyeon <wth2052@gmail.com>
2023-03-01 23:01:51 +0900

    ADD: Docker: MySQL, Redis, RabbitMQ

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './global/util/swagger/swagger-mainpage';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
