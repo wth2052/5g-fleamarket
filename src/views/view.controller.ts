ce5d56db277eb69070b7aa168bfa153b6d0a25c1
Seop0728 <ms_sh0728@naver.com>
2023-03-07 10:57:05 +0900

    feat: Login 완성 #6

---
+++ src/views/view.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import {Public} from "../global/common/decorator/skip-auth.decorator";
+
+@Controller('view')
+export class ViewController {
+  @Public()
+  @Get()
+  @Render('login.ejs')
+  async view() {
+    return { name: 'peter', age: 28, job: 'software engineer'};
+  }
+
+  @Get('index')
+  @Render('index.ejs')
+  async index(){
+
+  }
+}
