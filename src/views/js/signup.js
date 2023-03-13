72a90b0b1a6ee4815a7e0fc9b94b84628b1d429e
“Woo TaeHyeon <wth2052@gmail.com>
2023-03-13 23:44:10 +0900

    ADD: 프론트 이메일 타이머 두번클릭시 고장 이슈 해결 #74

--- src/views/js/signup.js
+++ src/views/js/signup.js
@@ -2,7 +2,7 @@ let regEmail =
   /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/; // 이메일 주소 길이까지 확실한 검증
 let regNickname = /^[가-힣]+$/; // 한글만
 // 우편번호 찾기 화면을 넣을 element
-let isRunning = false;
+
 //   메일 전송
 function sendMail(event) {
   const email = document.getElementById('email').value;
@@ -16,10 +16,10 @@ function sendMail(event) {
   alert('메일이 성공적으로 전송되었습니다. 인증번호의 유효기간은 10분입니다.');
 }
 function startTimer() {
-  isRunning = true;
   let time = 600; //기준시간 작성
   let min = ''; //분
   let sec = ''; //초
+  let currentTimer = null;
   //setInterval(함수, 시간) : 초마다 실행
   let x = setInterval(function () {
     //parseInt() : 정수를 반환
@@ -27,6 +27,12 @@ function startTimer() {
     sec = time % 60; //나머지를 계산
     document.getElementById('mailTimer').innerHTML = min + '분' + sec + '초';
     time--;
+    document
+      .getElementById('emailButton')
+      .addEventListener('click', function () {
+        clearInterval(x);
+        startTimer();
+      });
     //타임아웃 시
     if (time < 0) {
       clearInterval(x); //setInterval() 실행을 끝냄
