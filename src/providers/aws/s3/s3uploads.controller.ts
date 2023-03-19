48d4eb6df9a1c6629837ba23cc058c674d5c3359
“Woo TaeHyeon <wth2052@gmail.com>
2023-03-19 18:06:05 +0900

    ADD: S3Multer ImageUpload를 위한 초기세팅 #119

---
+++ src/providers/aws/s3/s3uploads.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import * as AWS from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('uploads')
+export class S3uploadsController {
+  constructor(private readonly configService: ConfigService) {}
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    AWS.config.update({
+      region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+    try {
+      const key = `${Date.now() + file.originalname}`;
+      // AWS 객체 생성
+      const upload = await new AWS.S3()
+        .putObject({
+          Key: key,
+          Body: file.buffer,
+          Bucket: this.configService.get('AWS_BUCKET_NAME'),
+        })
+        .promise();
+
+      const imgurl = this.configService.get('AWS_CLOUDFRONT') + key;
+      return Object.assign({
+        statusCode: 201,
+        message: `이미지 등록 성공`,
+        data: { url: imgurl },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
---
+++ src/providers/aws/s3/s3uploads.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class S3uploadsService {
+  private readonly s3;
+
+  constructor(private readonly configService: ConfigService) {
+    AWS.config.update({
+        region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+    this.s3 = new AWS.S3();
+  }
+
+  async uploadImage(file: Express.Multer.File) {
+    const key = `${Date.now() + file.originalname}`;
+    const params = {
+      Bucket: this.configService.get('AWS_BUCKET_NAME'),
+      ACL: 'private',
+      Key: key,
+      Body: file.buffer,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.s3.putObject(params, (err, data) => {
+        if (err) reject(err);
+        resolve(key);
+      });
+    });
+  }
+}
